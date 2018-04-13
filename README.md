5d10c7a44a2189fad151ca516cf75a2e51d62018
stakiran <stakiran@users.noreply.github.com>
2018-04-13 20:54:21 +0900

    first commit.

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # qiita_stock_canceler
-Qiita v2 API を使って自身のストックを全て消去する Python スクリプト 
+Qiita v2 API を使って自身のストックを全て消去する Python スクリプト
+
+# Requirement
+
+- Windows 7+
+- Python 3.6
+- requests ライブラリ
+
+# Installation
+
+- `git clone https://github.com/stakiran/qiita_stock_canceler`
+- `cd qiita_stock_canceler`
+- `copy stock_canceler.bat.sample stock_canceler.bat`
+- `stock_canceler.bat` を開き、QIITA_ACCESS_TOKEN 環境変数にアクセストークンを設定する
+  - アクセストークンは [Qiitaにログイン後、設定画面から発行できます](https://qiita.com/settings/applications)
+  - **read_qiita** と **write_qiita** の二つが必要です
+- プロキシが必要なら HTTPS_PROXY 環境変数もセットする
+  - 例: `set HTTPS_PROXY=https://(IP):(PORT)`
+- `stock_canceler.bat` を実行する
+
+# Demo
+
+当方の Qiita アカウントで実行してみた例です。655件のストックを消去しています。
+
+```
+$ stock_canceler.py
+getting between 1 to 101...
+getting between 101 to 201...
+getting between 201 to 301...
+getting between 301 to 401...
+getting between 401 to 501...
+getting between 501 to 601...
+getting between 601 to 701...
+1/655 TITLE:Windows環境＋node.js＋mocha＋istanbulでUTしてコードカバレッジを取る...
+2/655 TITLE:grunt+istanbul+mochaでNode.jsのテスト＆カバレッジ計測を行う...
+3/655 TITLE:システムで「性別」の情報を扱う前に知っておくべきこと...
+...
+653/655 ID:2015年センター試験数学IAを全てプログラム(Python)で解く...
+654/655 ID:100万倍速いプログラムを書く...
+655/655 ID:いい結婚相手を見つける最適な方法を検証してみた...
+Fin.
+
+$ stock_canceler.py
+getting between 1 to 101...
+Fin.
+```
+
+# 注意事項
+
+ストック消去は 1 件につき 1 リクエストを行うため、ストック数が多い場合は **Rate Limit を使い切らないよう** ご注意ください。Qiita API v2 の Rate Limit は 2018/04/13 現在で [1000リクエスト/時](https://qiita.com/api/v2/docs#%E5%88%A9%E7%94%A8%E5%88%B6%E9%99%90) です。
+
+# License
+
+[MIT License](LICENSE)
+
+# Author
+
+[stakiran](https://github.com/stakiran)
---
+++ stock_canceler.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+import json
+import os
+import sys
+
+import requests
+
+def abort(msg):
+    print('Error!: {0}'.format(msg))
+    sys.exit(1)
+
+def get(url, params, headers):
+    r = requests.get(url, params=params, proxies=proxies, headers=headers)
+    return r
+
+def delete(url):
+    r = requests.delete(url, proxies=proxies, headers=headers)
+    return r
+
+def print_response(r, title=''):
+    c = r.status_code
+    h = r.headers
+    print('{0} Response={1}, Detail={2}'.format(title, c, h))
+
+def assert_response(r, title=''):
+    c = r.status_code
+    h = r.headers
+    if c<200 or c>299:
+        abort('{0} Response={1}, Detail={2}'.format(title, c, h))
+
+class Stock:
+    def __init__(self, d):
+        self.id     = d['id']
+        self.title  = d['title']
+
+def get_stocks(target_userid, page, per_page):
+    url = 'https://qiita.com/api/v2/users/{}/stocks'.format(target_userid)
+    params = {
+        'page'     : page,
+        'per_page' : per_page,
+    }
+    r = get(url, params, headers)
+
+    items = r.json()
+    stocks = []
+    is_last = False
+
+    # int(r.headers['Total-Count']) 見た方が良い?
+    # でも len(items) と Total-Count が食い違うケースはそうはないと思う(思いたい)ので
+    # とりあえず len で判定.
+    if len(items) < per_page:
+        is_last = True
+
+    for i,item in enumerate(items):
+        stock = Stock(item)
+        stocks.append(stock)
+
+    return [stocks, is_last]
+
+def cancel_stock(item_id):
+    url = 'https://qiita.com/api/v2/items/{}/stock'.format(item_id)
+    r = delete(url)
+    return r
+
+MYDIR = os.path.abspath(os.path.dirname(__file__))
+
+proxies = {
+    "http": os.getenv('HTTP_PROXY'),
+    "https": os.getenv('HTTPS_PROXY'),
+}
+token = os.getenv('QIITA_ACCESS_TOKEN')
+headers = {
+    'content-type'  : 'application/json',
+    'charset'       : 'utf-8',
+    'Authorization' : 'Bearer {0}'.format(token)
+}
+
+# 1/2 Get all my stock ID
+# -----------------------
+
+PAGE_START_NO = 1
+target_userid = 'sta'
+per_page = 100
+
+cur_page = PAGE_START_NO
+all_stocks = []
+
+while True:
+    a = (cur_page-1)*per_page + PAGE_START_NO
+    b = a + per_page
+    print('getting between {} to {}...'.format(a, b))
+
+    stocks, is_last = get_stocks(target_userid, cur_page, per_page)
+    all_stocks.extend(stocks)
+
+    if is_last:
+        break
+
+    cur_page += 1
+
+#for i,stock in enumerate(all_stocks):
+#    print('{} ID:{}, {}'.format(i+1, stock.id, stock.title))
+
+# 2/2 delete all stock
+# --------------------
+
+total_count = len(all_stocks)
+for i,stock in enumerate(all_stocks):
+    target_id = stock.id
+    caption = '{}/{} TITLE:{}'.format(i+1, total_count, stock.title)
+
+    print('{}...'.format(caption))
+    r = cancel_stock(target_id)
+    assert_response(r, title=caption)
+
+print('Fin.')
